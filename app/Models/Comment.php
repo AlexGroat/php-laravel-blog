ee23757571bd11b9b7e8e37b89b03e3c9e64e7e2
Alex Groat <alex.groat.95@gmail.com>
2022-03-28 16:56:38 +1030

    make the comments section dynamic timestamp 3:56

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+
 
 class Comment extends Model
 {
     use HasFactory;
+
+    public function post() // name of method important
+    // laravel thinks foreign key is probably post_id
+    {
+        return $this->belongsTo(Post::class);
+    
+    }
+
+    public function author() // whereas here laravel will think the 
+    // foreign key is author_id
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    
+    }
 }
