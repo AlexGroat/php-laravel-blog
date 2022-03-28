19d686c8822e556e40ccefc23b468169ced11d42
Alex Groat <alex.groat.95@gmail.com>
2022-03-29 08:43:36 +1030

    end of episode 54 making the comments dynamic

--- resources/views/components/comment.blade.php
+++ resources/views/components/comment.blade.php
@@ -1,17 +1,15 @@
+@props(['comment'])
 <article class="flex bg-gray-100 border rounded-lg p-6 space-x-4">
     <div style="flex-shrink: 0;">
-        <img src="https://i.pravatar.cc/60" alt="" width="60" height="60" class="rounded-xl">
+        <img src="https://i.pravatar.cc/60?u={{ $comment->id}}" alt="" width="60" height="60" class="rounded-xl">
     </div>
 
     <div>
         <header class="mb-6">
-            <h3 class="font-bold">John Doe</h3>
-            <time class="text-xs">8 months ago</time>
+            <h3 class="font-bold"> {{ $comment->author->username}}</h3>
+            <time class="text-xs">{{ $comment->created_at->diffForHumans()}}</time>
         </header>
-
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Vero similique soluta dolorum dolore error nesciunt,
-            assumenda, cum, deleniti saepe laudantium quas? Natus
-            reprehenderit minima esse voluptates fuga porro magni inventore?</p>
+ 
+        <p>{{ $comment->body }}</p>
     </div>
 </article>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -26,7 +26,7 @@
                             <svg width="22" height="22" viewBox="0 0 22 22" class="mr-2">
                                 <g fill="none" fill-rule="evenodd">
                                     <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
-                                    </path>
+                                </path>
                                     <path class="fill-current" d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z">
                                     </path>
                                 </g>
@@ -52,11 +52,9 @@
                 </div>
 
                 <section class="col-span-8 col-start-5 mt-10 space-y-6">
-                    <x-comment />
-                    <x-comment />
-                    <x-comment />
-                    <x-comment />
-                    <x-comment />
+                    @foreach ($post->comments as $comment)
+                    <x-comment :comment="$comment" />
+                    @endforeach
                 </section>
             </article>
         </main>
